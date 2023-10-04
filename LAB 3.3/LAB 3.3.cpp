e63697ce0ccfbd27f187af5aa9cd4e22ce6416c0
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-10-05 00:24:46 +0300

    first master commit

---
+++ LAB 3.3/LAB 3.3.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_03_3.cpp
+// Боцко Іван Віталійович
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 4
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= 0)
+		y = (-(R * x + 6 * R))/(6);
+	else
+		if (0 < x && x <= R)
+			y = -sqrt(R * R - x * x) ;
+		else
+			if (R < x && x <= 2 * R)
+				y = sqrt(R * R - (x-2 * R)*(x-2 * R));
+			else
+				y = R;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
